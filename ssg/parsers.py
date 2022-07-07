df3811ae353e09e0fa6a50977c54811d8d3f956b
Kata Könczöl <90618410+konczolkata@users.noreply.github.com>
2022-07-07 16:13:34 +0100

    Removing self.

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -13,11 +13,11 @@ class Parser:
         raise NotImplementedError
         
     def read(self, path):
-        with open(self.path, 'r') as file:
+        with open(path, 'r') as file:
             return file.read()
             
     def write(self, path, dest, content, ext = ".html"):
-        full_path = self.dest/path.with_suffix(ext).name
+        full_path = dest/path.with_suffix(ext).name
         with open(full_path) as file:
             self.content.write(file)
             
