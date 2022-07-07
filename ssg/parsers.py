46fa1d4613c4ae2ff2ebd61f0e4047bdb3aafb3c
Kata Könczöl <90618410+konczolkata@users.noreply.github.com>
2022-07-07 16:16:12 +0100

    Removing self. all over

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -17,19 +17,16 @@ class Parser:
             return file.read()
             
     def write(self, path, dest, content, ext = ".html"):
-        full_path = dest/path.with_suffix(ext).name
+        full_path = dest/path.with_suffix(ext).name #why not self.?
         with open(full_path) as file:
-            self.content.write(file)
+            content.write(file)
             
     def copy(self, path, source, dest):
-        copy2(self.path, self.dest/self.path.relative_to(self.source))
+        copy2(path, dest/path.relative_to(source))
         
         
     class ResourceParser:
         extensions = [".jpg", ".png", ".gif", ".css", ".html"]
         
-        def parse(self, path, source, dest):
-            self.path = Path(path)
-            self.source = Path(source)
-            self.dest = Path(dest)
+        def parse(self, path : Path, source : Path, dest : Path):
             copy(path, source, dest)
