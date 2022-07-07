aa6c6afd37a422844f7c228109d17327c5294c00
Kata Könczöl <90618410+konczolkata@users.noreply.github.com>
2022-07-07 12:44:24 +0100

    Changed Site to how the module expects

--- ssg.py
+++ ssg.py
@@ -3,7 +3,6 @@ from ssg.site import Site
 
 def main(source = "content", dest = "dist"):
     config = {"source": source, "dest": dest}
-    s1 = Site(**config)
-    s1.build()
+    Site(**config).build()
     
 typer.run(main)
