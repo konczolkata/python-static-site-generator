596c21f0692bc3dc576d04b772463ddcef905693
Kata Könczöl <90618410+konczolkata@users.noreply.github.com>
2022-07-19 20:18:35 +0100

    open for write

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -18,11 +18,11 @@ class Parser:
             
     def write(self, path, dest, content, ext = ".html"):
         full_path = dest/path.with_suffix(ext).name #why not self.?
-        with open(full_path) as file:
+        with open(full_path, "w") as file:
             content.write(file)
             
     def copy(self, path, source, dest):
-        copy2(path, dest/path.relative_to(source))
+        shutil.copy2(path, dest/path.relative_to(source))
         
         
     class ResourceParser:
