0a8298ddc44e515776debe6105d89ce2bf932d13
Kata Könczöl <90618410+konczolkata@users.noreply.github.com>
2022-07-19 20:29:06 +0100

    Subclass?

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -25,7 +25,7 @@ class Parser:
         shutil.copy2(path, dest/path.relative_to(source))
         
         
-    class ResourceParser:
+    class ResourceParser(Parser):
         extensions = [".jpg", ".png", ".gif", ".css", ".html"]
         
         def parse(self, path : Path, source : Path, dest : Path):
