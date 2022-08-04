746fc509527761a44a7d27b1e36cdb457ffe83b4
Kata Könczöl <90618410+konczolkata@users.noreply.github.com>
2022-08-04 14:43:45 +0100

    changing load var assignment

--- ssg/content.py
+++ ssg/content.py
@@ -9,7 +9,7 @@ class Content(Mapping):
     
     @classmethod
     def load(cls, string):
-        cls.__regex.split(string, 2) = _, fm, content
+        _, fm, content = cls.__regex.split(string, 2)
         load(fm, Loader = FullLoader)
         return cls(metadata, content)
         
