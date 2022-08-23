aa9fed3c3b48cacef50f4c155a642d881a0125d9
Kata Könczöl <90618410+konczolkata@users.noreply.github.com>
2022-08-23 16:50:03 +0100

    self.write()

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -42,7 +42,7 @@ class MarkdownParser(Parser):
     def parse(self, path, source, dest):
         content = Content.load(self.read(path))
         html = markdown(content.body)
-        dest = self.write(html)
+        dest.self.write(html)
         sys.stdout.write(r"\x1b[1;32m{} converted to HTML. Metadata: {}\n").format(path.name, content)
         
 class ReStructuredTextParser(Parser):
@@ -51,5 +51,5 @@ class ReStructuredTextParser(Parser):
     def parse(self, path, source, dest):
         content = Content.load(self.read(path))
         html = publish_parts(content.body, writer_name = "html5")
-        dest = sefl.write(html["html_body"])
+        dest.self.write(html["html_body"])
         sys.stdout.write(r"\x1b[1;32m{} converted to HTML. Metadata: {}\n").format(path.name, content)
