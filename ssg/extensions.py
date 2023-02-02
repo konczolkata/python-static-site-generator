62d0c00cf0499a28135994b5b1ac9f32a78fb232
Kata Könczöl <90618410+konczolkata@users.noreply.github.com>
2023-02-02 15:18:25 +0000

    Updating the site generator

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -2,15 +2,15 @@ import sys
 import importlib
 from pathlib import Path 
 
-def load_module(self, directory, name):
+def load_module(directory, name):
     sys.path.insert(0, directory)
     importlib.import_module(name)
     sys.path.pop(0)
 
-def load_directory(self, directory):
+def load_directory(directory):
     for path in directory.rglob('*.py'):
         load_module(directory.as_posix(), path.stem)
 
-def load_bundled(self):
-    directory = Path(__file__).parent + '/extensions'
+def load_bundled():
+    directory = Path(__file__).parent / 'extensions'
     load_directory(directory)
